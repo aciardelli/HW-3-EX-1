a5b1139240ed019580fc96840f05458c796e1c27
aciardelli <anthciardelli@gmail.com>
2024-10-09 11:22:01 -0500

    Creating Table

--- answers.sh
+++ answers.sh
@@ -6,3 +6,15 @@ INSERT INTO Students VALUES(10, 'Anthony', '21', 'M', 500);
 UPDATE Students SET Points = Points + 50 WHERE Name='Basma';
 UPDATE Students SET Points = Points - 50 WHERE Name='Alex';
 
+CREATE TABLE graduates(
+    ID INTEGER  PRIMARY KEY AUTOINCREMENT,
+    Name  TEXT NOT NULL UNIQUE,
+    Age INTEGER,
+	Gender TEXT,
+	Points INTEGER,
+	Graduation TEXT
+);
+
+INSERT INTO graduates(Name, Age, Gender, Points) SELECT Name, Age, Gender, Points FROM Students WHERE Students.Name='Layal';
+UPDATE graduates SET Graduation='08-09-2018' WHERE Name='Layal';
+DELETE FROM Students WHERE Name='Layal';
